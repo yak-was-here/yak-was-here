857bdac6263deda599e8c9bda33a53e5d5115674
Isaac 'Yak' L <i@isaacyakl.com>
2018-05-06 21:26:36 -0700

    Update to new domain in jBir

--- js/jBir.js
+++ js/jBir.js
@@ -11,7 +11,7 @@ var numberOfWallpapers = 9;
 // Set the path to the directory of all the wallpapers
 // For example: absolute "http://www.example.com/images/wallpapers/" or relative "./images/wallpapers/" ending with a forward slash
 // Absolute is recommended so that this script will work on pages in subdirectories.
-var wallpaperPath = "http://www.izacktheyak.com/images/wallpaper_bin/";
+var wallpaperPath = "http://www.isaacyakl.com/images/wallpaper_bin/";
 //var wallpaperPath = "./images/wallpaper_bin/";
 
 // Array of all the wallpaper files
@@ -36,10 +36,10 @@ function setRandomWallpaper(newWallpaperPath)
 {
 	// Get a random number between 0 & the number of wallpapers
 	var randImgNum = Math.floor(Math.random() * (numberOfWallpapers - 0)) + 0;
-	
+
 	// Variable to hold location of next wallpaper
 	var image;
-	
+
 	// This if statement was included so that the path to the wallpapers
 	// directory could be updated if the path was not originally absolute
 	// (e.g. it was set as: ./folder/wallpapers instead of http://www.example.com/folder/wallpapers)
@@ -53,8 +53,8 @@ function setRandomWallpaper(newWallpaperPath)
 		// Build location of next wallpaper
 		image = wallpaperPath + imageFiles[randImgNum];
 	}
-	
-	
+
+
 	// Set new background image
 	document.body.style.backgroundImage = "url('" + image + "')";
 }
