1d3694ab8e735a0c1adcd0d03783487694dff5ce
Isaac "Yak" L <i@isaacyakl.com>
2021-08-12 23:34:51 -0700

    Add NavBar Link around branding and missing a tags

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,32 +1,44 @@
-import PropTypes from 'prop-types'
-import Link from 'next/link'
+import PropTypes from "prop-types";
+import Link from "next/link";
 
-const NavBar = ({active}) => {
-    return (
-        <nav className="no-select">
-            <div className="branding-container">
-                <a href="/">
-                    <img src="/img/yak-logo.svg" alt="yak Logo" title="Home" className="logo"/>
-                    <span className="site-wordmark">Yak</span>
-                </a>
-            </div>
-            <ul className="nav-menu">
-                <Link href="/"><li className={`nav-menu-item ${active === "portfolio" ? "active" : ""}`}>Portfolio</li></Link>
-                <Link href="/experience"><li className={`nav-menu-item ${active === "experience" ? "active" : ""}`}>Experience</li></Link>
-                <Link href="/#hire"><li className={`nav-menu-item ${active === "hire" ? "active" : ""}`}>Hire</li></Link>
-                <Link href="/#contact"><li className={`nav-menu-item ${active === "contact" ? "active" : ""}`}>Contact</li></Link>
-            </ul>
-        </nav>
-    )
-}
+const NavBar = ({ active }) => {
+	return (
+		<nav className="no-select">
+			<Link href="/">
+				<a>
+					<div className="branding-container">
+						<img src="/img/yak-logo.svg" alt="yak Logo" title="Home" className="logo" />
+						<span className="site-wordmark">Yak</span>
+					</div>
+				</a>
+			</Link>
+			<ul className="nav-menu">
+				<Link href="/experience">
+					<a>
+						<li className={`nav-menu-item ${active === "experience" ? "active" : ""}`}>Experience</li>
+					</a>
+				</Link>
+				<Link href="/#hire">
+					<a>
+						<li className={`nav-menu-item ${active === "hire" ? "active" : ""}`}>Hire</li>
+					</a>
+				</Link>
+				<Link href="/#contact">
+					<a>
+						<li className={`nav-menu-item ${active === "contact" ? "active" : ""}`}>Contact</li>
+					</a>
+				</Link>
+			</ul>
+		</nav>
+	);
+};
 
 NavBar.defaultProps = {
-    active: 'portfolio',
-}
+	active: "portfolio",
+};
 
 NavBar.propTypes = {
-    active: PropTypes.string,
-}
+	active: PropTypes.string,
+};
 
-
-export default NavBar
+export default NavBar;
