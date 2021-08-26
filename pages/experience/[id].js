f664346faf16f743cdb2e0c6372829558ee2a007
Isaac "Yak" L <i@isaacyakl.com>
2021-08-26 16:18:13 -0700

    Regret JSON file route

--- pages/experience/[id].js
+++ pages/experience/[id].js
@@ -4,13 +4,14 @@ import Header from "../../components/Header";
 import Section from "../../components/Section";
 import Footer from "../../components/Footer";
 import { getAllExperiences, getExperience } from "../../lib/experiences";
+import Link from "next/link";
 
 const experience = ({ experienceData }) => {
 	let xpObj = {};
 	let xpTitle = "Experience";
 	if (experienceData) {
 		xpObj = JSON.parse(experienceData.json);
-		xpTitle = xpObj.about.title;
+		xpTitle = xpObj.summary.title;
 	}
 
 	return (
@@ -19,7 +20,58 @@ const experience = ({ experienceData }) => {
 			<NavBar active="experience" />
 			<Header heading={xpTitle} />
 			<main role="main">
-				<Section>{xpObj.about.summary}</Section>
+				<Section>
+					<img src={`/img/experiences/${xpObj.summary.image}`} />
+					<h2>Summary</h2>
+					<ul className="experience-tags">
+						{xpObj.summary.tags.map((t) => {
+							return (
+								<Link href={`/experience?q=${t}`} key={t} passHref>
+									<a>
+										<li>{t}</li>
+									</a>
+								</Link>
+							);
+						})}
+					</ul>
+					<p>{xpObj.summary.body}</p>
+				</Section>
+				{xpObj.brief !== undefined && xpObj.brief !== "" ? (
+					<Section>
+						<h2>Brief</h2>
+						<p>{xpObj.brief}</p>
+					</Section>
+				) : (
+					``
+				)}
+				{xpObj.process !== undefined ? (
+					<Section>
+						<h2>Process</h2>
+						{xpObj.process.sections.map((s, i) => {
+							return (
+								<div key={i}>
+									{s.title !== "" ? <h3>{s.title}</h3> : ``}
+									{s.image !== "" ? <img src={`/img/experiences/${s.image}`} /> : ``}
+									{s.body !== "" ? <p>{s.body}</p> : ``}
+								</div>
+							);
+						})}
+					</Section>
+				) : (
+					``
+				)}
+				<Section>
+					<h2>Result</h2>
+					{xpObj.result.sections.map((s, i) => {
+						return (
+							<div key={i}>
+								{s.title !== "" ? <h3>{s.title}</h3> : ``}
+								{s.image !== "" ? <img src={`/img/experiences/${s.image}`} /> : ``}
+								{s.body !== "" ? <p>{s.body}</p> : ``}
+							</div>
+						);
+					})}
+				</Section>
 			</main>
 			<Footer />
 		</>
