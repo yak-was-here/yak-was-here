5d381c26ac46ca425416bad1c8edbc5b201646b5
Isaac "Yak" L <i@isaacyakl.com>
2021-12-21 23:54:22 -0800

    Update Skills

--- components/Skills.js
+++ components/Skills.js
@@ -7,34 +7,38 @@ const Skills = () => {
 			<div className="skills-container">
 				<ul className="skills">
 					<h3>Web Tech</h3>
-					<li>React.js</li>
 					<li>Next.js</li>
+					<li>React.js</li>
 					<li>jQuery</li>
-					<li>SASS / PostCSS</li>
 					<li>Shopify Liquid</li>
+					<li>TailwindCSS</li>
+					<li>Bootstrap</li>
 				</ul>
 				<ul className="skills">
 					<h3>Tools</h3>
-					<li>Git / GitHub</li>
+					<li>Git</li>
+					<li>GitHub</li>
 					<li>NPM</li>
+					<li>Scrum</li>
 					<li>Figma</li>
 					<li>Adobe Photoshop</li>
-					<li>Adobe Illustrator</li>
 				</ul>
 				<ul className="skills">
 					<h3>Platforms</h3>
-					<li>AWS</li>
-					<li>Heroku</li>
+					<li>Vercel</li>
 					<li>Shopify</li>
-					<li>WordPress</li>
 					<li>WooCommerce</li>
+					<li>WordPress</li>
+					<li>Heroku</li>
+					<li>AWS</li>
 				</ul>
 				<ul className="skills">
 					<h3>Marketing</h3>
 					<li>SEO</li>
-					<li>Facebook Ads</li>
+					<li>Facebook/Instagram Ads</li>
 					<li>Google Analytics</li>
 					<li>Google Console</li>
+					<li>Moz Pro</li>
 				</ul>
 			</div>
 		</Section>
