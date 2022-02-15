a06cb3bc194ac452c33d124e011f3bc2bc012ffe
Isaac "Yak" L <i@isaacyakl.com>
2022-02-15 13:50:07 -0800

    Revert font import to BaseMeta component

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,8 @@
 import "../styles/globals.css";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			<Head>
-				{/* Fonts */}
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-				<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto:ital,wght@0,100;0,300;0,400;0,700;1,400&display=swap" rel="stylesheet" />
-			</Head>
 			<Component {...pageProps} />
 		</>
 	);
