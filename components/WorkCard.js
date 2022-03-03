153dd2dd55ac9fc9ea5006e6f851b1a157486807
Isaac "Yak" L <i@isaacyakl.com>
2022-03-03 12:44:11 -0800

    Make whole work card clickable

--- components/WorkCard.js
+++ components/WorkCard.js
@@ -3,23 +3,17 @@ import Link from "next/link";
 
 const WorkCard = ({ id, title, date, image, summary }) => {
 	return (
-		<div className="work-card">
-			<Link href={`/work/${id}`} passHref>
-				<a>
-					<Image className="word-card-image" src={`/img/work/${image}`} layout="responsive" width="100%" height="60vh" objectFit="cover" objectPosition="center top" alt={title} />
-				</a>
-			</Link>
-			<div className="work-card-border"></div>
-			<div className="work-card-summary">
-				<Link href={`/work/${id}`} passHref>
-					<a>
-						<h3>{title}</h3>
-					</a>
-				</Link>
-				<h4 className="work-date">{date}</h4>
-				<p className="summary-body" dangerouslySetInnerHTML={{ __html: summary }}></p>
+		<Link href={`/work/${id}`} passHref>
+			<div className="work-card">
+				<Image className="word-card-image" src={`/img/work/${image}`} layout="responsive" width="100%" height="60vh" objectFit="cover" objectPosition="center top" alt={title} />
+				<div className="work-card-border"></div>
+				<div className="work-card-summary">
+					<h3>{title}</h3>
+					<h4 className="work-date">{date}</h4>
+					<p className="summary-body" dangerouslySetInnerHTML={{ __html: summary }}></p>
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 };
 
