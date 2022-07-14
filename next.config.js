f51f205406848c36fe9543bc913845641219b496
Isaac "Yak" L <i@isaacyakl.com>
2022-07-14 08:59:10 -0700

    Add redirects for old website pages

--- next.config.js
+++ next.config.js
@@ -4,4 +4,34 @@ module.exports = {
 		defaultLocale: "en",
 	},
 	reactStrictMode: true,
+
+	async redirects() {
+		return [
+			{
+				source: "/projects",
+				destination: "/work",
+				permanent: true,
+			},
+			{
+				source: "/projects/map-training",
+				destination: "/work/map-training",
+				permanent: true,
+			},
+			{
+				source: "/projects/nah-son-free-pie-gaming",
+				destination: "/work/nsfpgaming",
+				permanent: true,
+			},
+			{
+				source: "/projects/yourpbfriend",
+				destination: "/work/yourpbfriend",
+				permanent: true,
+			},
+			{
+				source: "/projects/k9-web-protection-password-cracker",
+				destination: "https://github.com/isaacyakl/k9wppc",
+				permanent: true,
+			},
+		];
+	},
 };
