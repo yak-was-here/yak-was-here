be654e8ff491bd033fd14a164de9b40d2e2247f6
Isaac "Yak" L <i@isaacyakl.com>
2022-07-19 13:57:37 -0700

    Use nextjs-google-analytics package

--- components/BaseMeta.js
+++ components/BaseMeta.js
@@ -59,18 +59,6 @@ function BaseMeta({ title, desc, author, robots, googlebot, shareURL, shareImg, 
 			<meta property="og:url" content={shareURL} />
 			<meta property="og:image" content={shareImg} />
 			<meta property="og:description" content={desc} />
-
-			{/* Global site tag (gtag.js) - Google Analytics */}
-			<Script src="https://www.googletagmanager.com/gtag/js?id=G-KTQBNQ49BV" strategy="afterInteractive" />
-			<Script id="google-analytics" strategy="afterInteractive">
-				{`
-					window.dataLayer = window.dataLayer || [];
-					function gtag(){dataLayer.push(arguments);}
-					gtag('js', new Date());
-
-					gtag('config', 'G-KTQBNQ49BV');
-				`}
-			</Script>
 		</Head>
 	);
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import "../styles/globals.css";
+import { GoogleAnalytics, usePageViews } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }) {
+	usePageViews();
+
 	return (
 		<>
+			<GoogleAnalytics />
 			<Component {...pageProps} />
 		</>
 	);
