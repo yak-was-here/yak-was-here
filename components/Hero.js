49e5888f70380ab00f67a9399637ed6783446864
Isaac "Yak" L <i@isaacyakl.com>
2022-08-08 03:40:04 -0700

    Add 'Accepting Work' notification to homepage

--- components/Hero.js
+++ components/Hero.js
@@ -2,9 +2,25 @@ import ContactIcons from "./ContactIcons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
-import { fName, lName, title, location } from "../data/meta";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faBan } from "@fortawesome/free-solid-svg-icons";
+import { fName, lName, title, location, acceptingWork } from "../data/meta";
+import EmailLink from "./EmailLink";
 
 function Hero() {
+	const renderWorkStatus = () => {
+		if (acceptingWork) {
+			return (
+				<EmailLink>
+					<div className="work-status success">
+						<FontAwesomeIcon icon={faCheck} />
+						Accepting work
+					</div>
+				</EmailLink>
+			);
+		}
+	};
+
 	return (
 		<header className="hero">
 			<div className="header-container max-page-width">
@@ -22,6 +38,7 @@ function Hero() {
 					{location}
 				</div>
 				<ContactIcons id="social" />
+				{renderWorkStatus()}
 			</div>
 		</header>
 	);
