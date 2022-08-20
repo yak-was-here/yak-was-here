b543058308568454cdea76905f270e0814535d08
Isaac "Yak" L <i@isaacyakl.com>
2022-08-19 20:52:14 -0600

    Create basic organization for services

---
+++ components/ServicePackage.js
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import CtaBtn from "./CtaBtn";
+
+function ServicePackage({ children, name, href, price }) {
+	const getCTABtn = () => {
+		if (href === null || href === "") return <CtaBtn text={"Purchase"} />;
+		else <CtaBtn text={"Purchase"} href={href} />;
+	};
+
+	return (
+		<div className="service-package">
+			<h2>{name}</h2>
+			{children}
+			<p style={{ textAlign: "right" }}>{price}</p>
+			<p className="text-center">{getCTABtn()}</p>
+		</div>
+	);
+}
+
+ServicePackage.defaultProps = {
+	name: "Sample package",
+	href: null,
+	price: "$0",
+};
+
+ServicePackage.propTypes = {
+	name: PropTypes.string,
+	href: PropTypes.string,
+	price: PropTypes.string,
+};
+
+export default ServicePackage;
--- pages/digital-marketing-services.js
+++ pages/digital-marketing-services.js
@@ -5,6 +5,7 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 import { fName, lName, nick } from "../data/meta";
+import ServicePackage from "../components/ServicePackage";
 
 function DigMktgServices() {
 	return (
@@ -19,77 +20,112 @@ function DigMktgServices() {
 			/>
 			<Header heading="Digital Marketing Services" />
 			<main>
-				<section>Best Deals section</section>
-				<div className="text-center" style={{ backgroundColor: "purple" }}>
-					<h3 style={{ color: "white", padding: "1em" }}>What can I do for you?</h3>
-				</div>
-
 				<section>
+					<p>Display three popular package options in a grid. Link to packages section at bottom of page.</p>
 					<ul>
-						<li>Web Design, Development, &amp; Management</li>
-						<li>Social Media Marketing &amp; Management</li>
-						<ul>
-							<li>Instagram</li>
-							<li>Facebook</li>
-							<li>YouTube</li>
-							<li>Twitter</li>
-							<li>TikTok</li>
-							<li>Yelp</li>
-							<li>Google Business</li>
-						</ul>
-						<li>Branding</li>
-						<ul>
-							<li>Social Media Branding</li>
-							<li>Brand Identity: Competitive Differentiation</li>
-						</ul>
-						<li>Ecommerce</li>
-						<ul>
-							<li>Product Management: Addition, Updating</li>
-							<li>Platforms</li>
+						<li>Pack1</li>
+						<li>Pack2</li>
+						<li>Pack3</li>
+					</ul>
+				</section>
+				<section>
+					<hr />
+					<h2>Website Services</h2>
+					<ul>
+						<li>Web Design</li>
+						<li>Web Development</li>
+						<li>Web Content Development</li>
+						<li>Web Maintenance</li>
+					</ul>
+					<h2>Ecommerce Services</h2>
+					<ul>
+						<li>Product Management: Addition, Updating</li>
+						<li>Sales Channel Integration and Management</li>
+						<li>
+							Platforms
 							<ul>
 								<li>Shopify</li>
 								<li>Amazon</li>
 								<li>eBay</li>
-								<li>Walmart</li>
 								<li>BigCommerce</li>
-								<li>Magento</li>
 								<li>Volusion</li>
 								<li>WooCommerce</li>
 							</ul>
-						</ul>
-						<li>Search Engine Optimization (SEO)</li>
-						<ul>
-							<li>Keyword Targeting</li>
-							<li>Technical Optimization</li>
-							<li>Blog Article Writing</li>
-							<li>Quality Link Building</li>
-						</ul>
-						<li>Multi-Channel Online Marketing Strategies</li>
+						</li>
+					</ul>
+					<h2>Graphic Design Services</h2>
+					<ul>
+						<li>
+							Brand Identity
+							<ul>
+								<li>Logo Design</li>
+								<li>Competitive Differentiation</li>
+								<li>Value Communication</li>
+							</ul>
+						</li>
+						<li>Social Media Branding</li>
+						<li>Web Banner Ads</li>
+						<li>Business Cards</li>
+						<li>Fliers</li>
+					</ul>
+					<h2>Search Engine Optimization (SEO) Services</h2>
+					<ul>
+						<li>Keyword Targeting</li>
+						<li>Technical Optimization</li>
+						<li>Blog Article Writing / Copywriting</li>
+						<li>Quality Link Building</li>
+					</ul>
+					<h2>Social Media Services</h2>
+					<ul>
+						<li>Social Media Marketing</li>
+						<li>Social Media Management</li>
+						<li>Account / Profile Branding</li>
+						<li>Shopping / Product Integration</li>
+						<li>
+							Platforms
+							<ul>
+								<li>Instagram</li>
+								<li>Facebook</li>
+								<li>YouTube</li>
+								<li>Twitter</li>
+								<li>TikTok</li>
+								<li>Yelp</li>
+								<li>Google Business</li>
+							</ul>
+						</li>
+					</ul>
+				</section>
+				<h2 className="section-outside-heading" id="packages">
+					Packages
+				</h2>
+				<section>
+					<p>Packages in detail.</p>
+					<ServicePackage name={"Multi-Channel Marketing Package"} price={"$2,500"}>
 						<ul>
 							<li>Search Engine Marketing (SEM)</li>
 							<li>Social Media Ads</li>
-							<li>Email Outreach</li>
+							<li>Email Marketing</li>
 							<li>Retargeting</li>
 						</ul>
-						<li>Graphic Design</li>
+					</ServicePackage>
+					<ServicePackage name={"Landing Page Marketing Package"} price={"$4,000"}>
 						<ul>
-							<li>Logo Design</li>
-							<li>Web Banners</li>
-							<li>Fliers</li>
-							<li>Business Cards</li>
-							<li>Ads</li>
+							<li>Multi-Channel Marketing Package</li>
+							<li>Landing Page</li>
 						</ul>
-					</ul>
-				</section>
-				<section>
-					<h2>Packages</h2>
+					</ServicePackage>
+					<ServicePackage name={"Website Marketing Package"} price={"$3,000"}>
+						<ul>
+							<li>Website or Ecommerce Store</li>
+						</ul>
+					</ServicePackage>
 				</section>
 				{/* 
 					- https://www.shopify.in/blog/ecommerce-marketing-analytics-how-to-measure-the-success-of-your-marketing-campaigns
 					- https://www.singlegrain.com/blog-posts/link-building/the-ultimate-guide-to-link-building-with-content/
 					- https://rosshill.ca/
  				*/}
-				<section style={{ textAlign: "center" }}>
+				<section className="text-center">
 					<hr />
 					<h3>Take your business to the next level.</h3>
 					<CtaBtn />
