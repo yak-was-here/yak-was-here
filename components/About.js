26a4e59033cbc727710acdd32e6ab0c13886273d
Isaac "Yak" L <i@isaacyakl.com>
2022-08-20 09:37:44 -0600

    Add informal blurb to index

--- components/About.js
+++ components/About.js
@@ -4,6 +4,7 @@ function About() {
 	return (
 		<div className="intro">
 			<h2>About</h2>
+			<p>Hi! My name is Isaac, but most people call me "yak".</p>
 			<p dangerouslySetInnerHTML={{ __html: intro }}></p>
 		</div>
 	);
