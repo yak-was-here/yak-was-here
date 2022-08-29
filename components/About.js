eb9abd15ef4ac0210f831909fa21634974f3c1cc
Isaac "Yak" L <i@isaacyakl.com>
2022-08-29 16:43:25 -0600

    Add ability to hide About heading

--- components/About.js
+++ components/About.js
@@ -1,12 +1,24 @@
 import { intro } from "../data/meta";
+import PropTypes from "prop-types";
 
-function About() {
+function About({ showHeading }) {
+	const renderHeading = () => {
+		return showHeading ? <h2>About</h2> : "";
+	};
 	return (
 		<div className="intro">
-			<h2>About</h2>
+			{renderHeading()}
 			<p dangerouslySetInnerHTML={{ __html: intro }}></p>
 		</div>
 	);
 }
+
+About.defaultProps = {
+	showHeading: true,
+};
+
+About.propTypes = {
+	showHeading: PropTypes.bool,
+};
 
 export default About;
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 			<Hero />
 			<main className="max-page-width">
 				<section>
-					<About />
+					<About showHeading={false} />
 					<CtaBtn text="Browse work" href="/work" />
 				</section>
 			</main>
