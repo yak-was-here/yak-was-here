be300ce34fc40ece8f3aef8e73b3a945fd7d96bd
Isaac "Yak" L <i@isaacyakl.com>
2022-11-14 14:30:51 -0800

    Codemod images in components

--- components/Branding.tsx
+++ components/Branding.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import logo from "../public/img/yak-logo.svg";
 import { nick } from "../data/meta";
 
