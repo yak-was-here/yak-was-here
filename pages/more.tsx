f06b802cce7c6b56185e035c42edb6dd15141ddc
Isaac "Yak" L <i@isaacyakl.com>
2022-11-14 21:27:34 -0800

    Change more page to include resumeTagline

--- pages/more.tsx
+++ pages/more.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 import BaseMeta from "../components/BaseMeta";
 import ContactIcons from "../components/ContactIcons";
 import NavBar from "../components/NavBar";
-import { fName, lName, nick, tagline } from "../data/meta";
+import { fName, lName, nick, resumeTagline } from "../data/meta";
 
 export default function More() {
 	return (
-        <div className="more-background">
+		<div className="more-background">
 			<BaseMeta title={`Links to ${nick}'s businesses, projects, and more.`} desc={`Browse a list of ${nick}'s businesses, projects, and more.`} />
 			<NavBar active="more" />
 			<main className="max-page-width more">
@@ -14,15 +14,13 @@ export default function More() {
 					<h1>
 						{fName} &quot;{nick}&quot; {lName}
 					</h1>
-					<p>{tagline}</p>
+					<p>{resumeTagline}</p>
 				</section>
 				<section className="more-link-container">
 					<Link href="/" className="btn cta more-links">
 						My Website
 					</Link>
-					<Link
-                        href="https://open.spotify.com/user/izacktheyak/playlists"
-                        className="btn cta more-links">
+					<Link href="https://open.spotify.com/user/izacktheyak/playlists" className="btn cta more-links">
 						Spotify Playlists
 					</Link>
 					<Link href="https://nsfpgaming.com" className="btn cta more-links">
@@ -39,5 +37,5 @@ export default function More() {
 				</section>
 			</main>
 		</div>
-    );
+	);
 }
