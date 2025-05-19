74fbe23af2c61c8e52a60a5004b32c96bbd75e2e
yak <yak@isaacyakl.com>
2025-05-19 14:28:31 -0700

    Remove accepting work status from hero

--- isaacyakl/components/Hero.tsx
+++ isaacyakl/components/Hero.tsx
@@ -21,29 +21,36 @@ function Hero() {
 	};
 
 	return (
-		<header className="min-h-screen">
-			<div className="max-width m-auto min-h-screen p-2 flex flex-col justify-center items-start overflow-hidden">
-				<h1 className="transition-all duration-100 ease text-[20vw] leading-[16vw] xl:text-[11vw] xl:leading-[8.8vw] py-2 -ml-[0.039em]">
-					{fName}
-					<br />
-					{lName}
-				</h1>
-				<hr className="m-0 w-[20vw] my-1" />
-				<div className="py-2 text-lg">
-					<div className="flex flex-nowrap mb-[0.33em]">
-						<FontAwesomeIcon icon={faBriefcase} className="h-[1.3em] w-[1.3em] mr-[0.33em]" />
-						<span className="inline-block h-[1.3em]">{title}</span>
-					</div>
-					<div className="flex flex-nowrap">
-						<FontAwesomeIcon icon={faMapMarkerAlt} className="h-[1.3em] w-[1.3em] mr-[0.33em]" />
-						<span className="inline-block h-[1.3em]">{location}</span>
-					</div>
-				</div>
-				<ContactIcons id="social" />
-				{renderWorkStatus()}
-			</div>
-		</header>
-	);
+        <header className="min-h-screen">
+            <div className="max-width m-auto min-h-screen p-2 flex flex-col justify-center items-start overflow-hidden">
+                <h1 className="transition-all duration-100 ease text-[20vw] leading-[16vw] xl:text-[11vw] xl:leading-[8.8vw] py-2 -ml-[0.039em]">
+                    {fName}
+                    <br />
+                    {lName}
+                </h1>
+                <hr className="m-0 w-[20vw] my-1" />
+                <div className="py-2 text-lg">
+                    <div className="flex flex-nowrap mb-[0.33em]">
+                        <FontAwesomeIcon
+                            icon={faBriefcase}
+                            className="h-[1.3em] w-[1.3em] mr-[0.33em]"
+                        />
+                        <span className="inline-block h-[1.3em]">{title}</span>
+                    </div>
+                    <div className="flex flex-nowrap">
+                        <FontAwesomeIcon
+                            icon={faMapMarkerAlt}
+                            className="h-[1.3em] w-[1.3em] mr-[0.33em]"
+                        />
+                        <span className="inline-block h-[1.3em]">
+                            {location}
+                        </span>
+                    </div>
+                </div>
+                <ContactIcons id="social" />
+            </div>
+        </header>
+    );
 }
 
 export default Hero;
