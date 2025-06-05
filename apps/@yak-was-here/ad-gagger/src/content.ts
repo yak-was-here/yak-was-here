2206f39dc84d876d1dc5b175fc44f361b0a9c243
yak <yak@isaacyakl.com>
2025-06-04 22:12:18 -0700

    WIP: b1de159 WIP scaffolding for ad-gagger

--- apps/@yak-was-here/ad-gagger/src/content.ts
+++ apps/@yak-was-here/ad-gagger/src/content.ts
@@ -1,17 +1,98 @@
-const twitchAdOverlaySelector = '[aria-label="Ad"]';
+type SiteConfiguration = {
+    uriMatcher: string;
+    adSelector: string;
+    parentToAdSelector: string;
+};
 
-function checkElement() {
-    const element = document.querySelector(twitchAdOverlaySelector);
-    if (element) {
+// All possible site configurations
+const siteConfigurations: SiteConfiguration[] = [
+    {
+        uriMatcher: 'https://www.twitch.tv/*',
+        adSelector: '[aria-label="Ad"]',
+        parentToAdSelector: '[data-a-target="player-above-the-fold"]',
+    },
+];
+
+function convertMatchPatternToRegExp(pattern: string): RegExp {
+    return new RegExp(
+        '^' + pattern.replace(/\*/g, '.*').replace(/[.]/g, '\\.') + '$'
+    );
+}
+
+/**
+ * Set site settings based on current URL
+ * @returns
+ */
+function setSiteSettings(): SiteConfiguration | null {
+    const currentUrl = window.location.href;
+
+    for (const siteConfiguration of siteConfigurations) {
+        if (
+            convertMatchPatternToRegExp(siteConfiguration.uriMatcher).test(
+                currentUrl
+            )
+        ) {
+            return siteConfiguration;
+        }
+    }
+
+    return null;
+}
+
+/**
+ * Checks if a DOM element exists
+ * @param selector
+ * @returns
+ */
+function checkForElement(selector: string): Element | null {
+    return document.querySelector(selector);
+}
+
+/**
+ * Processes changes to the DOM
+ * @param siteSettings
+ */
+function handleElementChange(siteSettings: SiteConfiguration) {
+    const element = checkForElement(siteSettings.adSelector);
+
+    // Element was added
+    if (element && !tabMuted) {
+        tabMuted = true;
+        console.log('Ad Gagger: Target element added', element);
         chrome.runtime.sendMessage({ action: 'mute' });
-    } else {
+    }
+    // Element was removed
+    else if (!element && tabMuted) {
+        tabMuted = false;
+        console.log('Ad Gagger: Target element removed', element);
         chrome.runtime.sendMessage({ action: 'unmute' });
     }
 }
 
-// Check once on load
-checkElement();
+// Keep track of tab mute state
+let tabMuted = false;
 
-// Mutations observer to detect changes in the DOM
-const observer = new MutationObserver(checkElement);
-observer.observe(document.body, { childList: true, subtree: true });
+window.addEventListener('load', () => {
+    const siteSettings: SiteConfiguration | null = setSiteSettings();
+
+    if (!siteSettings) {
+        console.error('Ad Gagger: No site settings found');
+    } else {
+        console.log('Ad Gagger: Site settings', siteSettings);
+    }
+
+    if (siteSettings) {
+        const observer = new MutationObserver(() => {
+            handleElementChange(siteSettings);
+        });
+
+        observer.observe(document.body, {
+            childList: true, // Watch for changes in direct children
+            subtree: true, // Watch for changes in all descendants
+            attributes: true, // Watch for attribute changes
+        });
+
+        // Initial check
+        handleElementChange(siteSettings);
+    }
+});
