2fdf500ce903c664ac0665419c275397781046fa
yak <yak@isaacyakl.com>
2025-10-05 00:27:32 -0700

    Simplify eslint config for now

--- apps/@yak-was-here/ad-gagger-wxt/eslint.config.mjs
+++ apps/@yak-was-here/ad-gagger-wxt/eslint.config.mjs
@@ -2,25 +2,6 @@ import baseConfig from '../../../eslint.config.mjs';
 import autoImports from './.wxt/eslint-auto-imports.mjs';
 
 export default [
-    ...baseConfig,
     autoImports,
-    {
-        files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-        settings: {
-            'import/resolver': {
-                typescript: {
-                    alwaysTryTypes: true,
-                    project: './tsconfig.json',
-                },
-            },
-        },
-        rules: {
-            'import/no-unresolved': [
-                'error',
-                {
-                    ignore: ['^@/', '^/'],
-                },
-            ],
-        },
-    },
+    ...baseConfig,
 ];
