06f75888fd4a613156c37be1255e0e0020101bf6
yak <yak@isaacyakl.com>
2025-10-04 23:58:03 -0700

    Use wxt auto icons

--- apps/@yak-was-here/ad-gagger-wxt/wxt.config.ts
+++ apps/@yak-was-here/ad-gagger-wxt/wxt.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'wxt';
-import packageJson from './package.json';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-    modules: ['@wxt-dev/module-react'],
+    modules: [
+        '@wxt-dev/module-react',
+        '@wxt-dev/auto-icons', // https://www.npmjs.com/package/@wxt-dev/auto-icons
+    ],
     manifest: {
-        name: "Ad Gagger",
-        description: packageJson.description,
-        version: packageJson.version,
+        name: 'Ad Gagger',
         permissions: ['storage'],
         host_permissions: ['<all_urls>'],
     },
